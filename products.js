ce5d5411a0d800e7deb46ab0e0952c79a66d27a4
Amb0yo10 <168243743+Amb0yo10@users.noreply.github.com>
2024-04-27 09:19:05 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,73 @@
+import product1 from './assets/1.jpg';
+import product2 from './assets/2.jpg';
+import product3 from './assets/3.jpg';
+import product4 from './assets/4.jpg';
+import product5 from './assets/5.jpg';
+import product6 from './assets/6.jpg';
+import product7 from './assets/7.jpg';
+import product8 from './assets/8.jpg';
+import product9 from './assets/9.jpg';
+import product10 from './assets/10.jpg';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'NBA 2k24',
+        price: 8.50,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: 'NBA 2k23',
+        price: 9.50,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: 'NBA 2k22',
+        price: 5.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: 'NBA 2k21',
+        price: 14.99,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: 'NBA 2k20',
+        price: 10.35,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: 'NBA 2k19',
+        price: 2.18,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: 'NBA 2k18',
+        price: 29.99,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: 'NBA 2k17',
+        price: 29.99,
+        productImage: product8,
+    },
+    {
+        id: 9,
+        productName: 'NBA 2k16',
+        price: 149.99,
+        productImage: product9,
+    },
+    {
+        id: 10,
+        productName: 'NBA 2k15',
+        price: 39.99,
+        productImage: product10,
+    }
+];
